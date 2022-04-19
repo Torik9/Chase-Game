40144dc517907923e67ce1b5e4aaf4476800e2d4
Codebuddy <80802610+teamcodebuddy@users.noreply.github.com>
2022-04-19 22:21:56 +0000

    Added chase game

--- main.py
+++ main.py
@@ -1,50 +1,158 @@
-# Tutorial: https://pygame-zero.readthedocs.io/en/stable/introduction.html
+#https://electronstudio.github.io/pygame-zero-book/chapters/chase.html
 
-# Docs:
-#https://pygame-zero.readthedocs.io/en/1.1/builtins.html
+# TODO explain global variables
+import random
+import math
 
-WIDTH = 500
-HEIGHT = 500
+jake_speed = 0.5
+finn_speed = 12
+gumball_speed = 13
+score = 0
 
-ball = Rect((200, 400), (20, 20))
-paddle = Rect((0,250), (20,60))
-paddle2 = Rect((480,240), (20,60))
+WIDTH = 1100
+HEIGHT = 619 
 
-velocity_x = 0
+background = Actor("background")
+finn = Actor("finn")
+finn.x = 200
+finn.y = 200
 
-velocity_y = 0
+coin = Actor("coin")
+coin.x = 356
+coin.y = 289
+
+jake = Actor("jake")
+jake.x = 560
+jake.y = 450
+
+gumball = Actor("gumball")
+gumball.x = 100
+gumball.x = 150
+
 
 def draw():
-    screen.clear()
-    screen.draw.filled_rect(ball, "red")
-    screen.draw.filled_rect(paddle, "white")
-    screen.draw.filled_rect(paddle2, "white")
+  screen.clear()
+  background.draw()
+  finn.draw() 
+  jake.draw()
+  coin.draw()
+  gumball.draw()
 
-def on_key_down(key):
-  if key == keys.UP:
-    paddle2.top = paddle2.top - 45
-  if key == keys.DOWN:
-    paddle2.bottom = paddle2.bottom + 45
-  if key == keys.W:
-    paddle.top = paddle.top - 45
-  if key == keys.S:
-    paddle.bottom = paddle.bottom + 45
-  
 def update():
-  global velocity_x, velocity_y
-  ball.x = ball.x + velocity_x
-  ball.y = ball.y + velocity_y
-  if ball.bottom > HEIGHT or ball.top < 0:
-    velocity_y = -velocity_y 
-  if ball.colliderect(paddle) or ball.colliderect(paddle2):
-    velocity_x = -velocity_x
-  if ball.right > WIDTH or ball.left < 0:
+  global jake_speed
+  global finn_speed
+  global gumball_speed
+  global score
+  
+  # Moving finn around the world
+  if keyboard.right:
+    finn.x = finn.x + finn_speed
+  if keyboard.left:
+    finn.x = finn.x - finn_speed
+  if keyboard.down:
+    finn.y = finn.y +finn_speed
+  if keyboard.up:
+    finn.y = finn.y - finn_speed
+  if finn.x > WIDTH:
+    finn.x = 0
+  if finn.x < 0:
+    finn.x = WIDTH
+  if finn.y < 0:
+    finn.y = HEIGHT
+  if finn.y > HEIGHT:
+    finn.y = 0
+
+  # Moving gumball 
+  if keyboard.d:
+    gumball.x = gumball.x + gumball_speed
+  if keyboard.a:
+    gumball.x = gumball.x - gumball_speed
+  if keyboard.s:
+    gumball.y = gumball.y + gumball_speed
+  if keyboard.w:
+    gumball.y = gumball.y - gumball_speed
+  if gumball.x > WIDTH:
+    gumball.x = 0
+  if gumball.x < 0:
+    gumball.x = WIDTH
+  if gumball.y < 0:
+    gumball.y = HEIGHT
+  if gumball.y > HEIGHT:
+    gumball.y = 0
+
+  # Moving jake 
+
+  # if jake is closer to finn 
+    # move to finn 
+  # else if jake is closer to gumball 
+    # move to gumball 
+
+  finn_distance = calculate_distance(finn, jake)
+  
+  gumball_distance = calculate_distance(gumball, jake)
+
+  if finn_distance < gumball_distance: 
+    # move jake to finn
+    if finn.x > jake.x:
+      jake.x = jake.x + jake_speed
+    if finn.x < jake.x:
+      jake.x =  jake.x - jake_speed
+    if finn.y > jake.y:
+      jake.y = jake.y + jake_speed
+    if finn.y < jake.y:
+      jake.y = jake.y - jake_speed
+  else: 
+    # move jake to gumball
+    if gumball.x > jake.x:
+      jake.x = jake.x + jake_speed
+    if gumball.x < jake.x:
+      jake.x =  jake.x - jake_speed
+    if gumball.y > jake.y:
+      jake.y = jake.y + jake_speed
+    if gumball.y < jake.y:
+      jake.y = jake.y - jake_speed 
+  
+  if finn.colliderect(coin):
+    coin.x = random.randint(1,WIDTH)
+    coin.y = random.randint(1,HEIGHT)
+    score = score + 1
+    print(score)
+    gumball_speed = gumball_speed - 0.6
+    jake_speed = jake_speed + 0.7
+
+  if gumball.colliderect(coin):
+    coin.x = random.randint(1,WIDTH)
+    coin.y = random.randint(1,HEIGHT)
+    score = score + 1
+    print(score)
+    finn_speed = finn_speed - 0.6
+    jake_speed = jake_speed + 0.7
+
+  if jake.colliderect(coin):
+    coin.x = random.randint(1, WIDTH)
+    coin.y = random.randint(1, HEIGHT)
+    score = score - 1
+    print(score)
+    finn_speed = finn_speed - 0.6
+    gumball_speed = gumball_speed - 0.6
+    jake_speed = jake_speed + 3
+    
+
+  if gumball.colliderect(finn):
+    finn_speed = finn_speed + 0.01
+    gumball_speed = gumball_speed + 0.01
+   
+  if finn.colliderect(jake):
     exit()
-  if paddle.bottom > HEIGHT:
-    paddle.bottom = HEIGHT
-  if paddle2.bottom > HEIGHT:
-    paddle2.bottom = HEIGHT
-  if paddle.top < 0:
-    paddle.top = 0
-  if paddle2.top < 0:
-    paddle2.top = 0
+  
+  if gumball.colliderect(jake):
+    exit()
+
+
+def calculate_distance(character1, character2):
+  
+  dx = character2.x - character1.x 
+  dy = character2.y - character1.y
+
+  distance = math.sqrt(pow(dx,2) + pow(dy,2))
+  return distance 
